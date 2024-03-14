12cbc74d78f53896f6cb41fb1cc5ff6bba317481
AndreSebastiao <andresebastiao755@gmail.com>
2024-03-14 12:22:00 +0000

    Exercise BetterDecorator done

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -6,7 +6,26 @@ namespace BetterDecorator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Print based on the different arguments
+            Console.WriteLine(Decor(args[0], char.Parse(args[1]), int.Parse(args[2])));
+        }
+
+        //Set the parameters for each argument
+        private static string Decor (string s, char c, int i)
+        {
+            string p = "";
+            string d = "";
+
+            //Specify the special character and the amount of times used
+            for (int j = 1; j <= i; ++j)
+            {
+                d += c;
+            }
+
+            //Form of the print
+            p += $"{d} {s} {d}";
+
+            return p;
         }
     }
 }
